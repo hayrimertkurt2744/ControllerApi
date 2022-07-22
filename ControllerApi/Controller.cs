072b926c62f75b2c9e6c1c10ebcaa7ffa1e80995
Hayri Mert kurt <hayrimertkurt@outlook.com>
2022-07-22 17:22:55 +0300

    Add project files.

---
+++ ControllerApi/Controller.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControllerApi
+{
+    public class Controller
+    {
+        Thread m_thread;
+        public delegate void CallbackMsg(int code, string msg);//async programming delegation purposes.
+        bool m_running = false;
+        CallbackMsg pCallback;
+        int Temperature = 20;
+
+        public Controller(CallbackMsg p) 
+        {
+            pCallback = p;
+        }
+        public void Start()
+        {
+            if (m_thread==null|| m_thread.IsAlive==false)
+            {
+                m_thread = new Thread(Process);
+                m_thread.Start();//it starts the thread and has nothing to do with the defined start function in this class
+            }
+            
+            //pCallback(1, "Started");
+        }
+
+        void Process()
+        {
+            m_running = true;
+            while (m_running)//when it is called by the start method,it'll update temp with checksensor method by checking the m_running value,if stop is pressed
+                             //m_running will be false and the process function will stop and will wait for a new start call.
+            {
+                Thread.Sleep(1000);
+                int temp=CheckSensor();
+                pCallback(10, temp.ToString());//m_running==false ,gets it out from while loop 
+            }
+            pCallback(0, "Stopped");//and do the things below the concearning cases in the form class using the pCallBack delegation.
+        }
+        int CheckSensor() 
+        {
+            Temperature++;
+            return Temperature;
+        }
+        public void Stop()
+        {
+            //pCallback(0, "Stopped");
+            m_running = false;
+            while (m_thread.IsAlive==true)
+            {
+                Thread.Sleep(10);//waits a little
+            }
+        }
+    }
+}
---
+++ HostApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ControllerApi;
+
+namespace HostApp
+{
+    public partial class Form1 : Form
+    {
+        Controller m_api;
+        public Form1()
+        {
+            Controller.CallbackMsg p = new Controller.CallbackMsg(MyCallBack);//makes it do something after receiving it.
+            m_api = new Controller(p);
+
+            InitializeComponent();
+        }
+        void MyCallBack(int code, string msg) 
+        {
+            switch (code)
+            {   case 0:
+                case 1:
+                    if (label1.InvokeRequired)//in order to update the lable in every change of it.
+                    {
+                        label1.BeginInvoke((MethodInvoker)delegate(){
+                            this.label1.Text = msg;
+                        });
+                    }
+                    else
+                    {
+                        label1.Text = msg;
+                    }
+                    //MessageBox.Show(msg);
+                    break;
+                case 10:
+                    if (label3.InvokeRequired)//in order to update the lable in every change of it.
+                    {
+                        label3.BeginInvoke((MethodInvoker)delegate () {
+                            this.label3.Text = msg;
+                        });
+                    }
+                    else
+                    {
+                        label3.Text = msg;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_api.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m_api.Stop();
+        }
+    }
+}
